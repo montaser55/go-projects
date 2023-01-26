e1a6c9c18a0552954d7554b6755a02cdb72f00ba
ruhit3 <ruhit1011@gmail.com>
2023-01-26 11:37:44 +0600

    Add init-disable-tfa and disable-tfa APIs

--- two-factor-authentication-system/pkg/models/requests/tfa-enable-request.go
+++ two-factor-authentication-system/pkg/models/requests/tfa-enable-request.go
@@ -8,3 +8,15 @@ type TfaEnableRequest struct {
 	ReferenceId    string               `json:"referenceId"`
 	Otp            string               `json:"otp"`
 }
+
+type TfaDisableInitRequest struct {
+	UserId         int64                `json:"userId"`
+	TfaChannelType enums.TfaChannelType `json:"tfaChannelType"`
+}
+
+type TfaDisableRequest struct {
+	UserId         int64                `json:"userId"`
+	TfaChannelType enums.TfaChannelType `json:"tfaChannelType"`
+	ReferenceId    string               `json:"referenceId"`
+	Otp            string               `json:"otp"`
+}
